8e806b5bc8818f38eb9d52a1bba7899d4f8f594b
Praveen Kumar <praveenbapuram.explore@gmail.com>
2020-04-04 16:55:18 +0530

    Converting Visit as entity.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/model/Visit.java
@@ -1,13 +1,20 @@
 package guru.springframework.sfgpetclinic.sfgpetclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
 
-
+@Entity
+@Table(name="visit")
 public class Visit extends BaseEntity {
+
+    @Column(name="date")
     private LocalDate localDate;
+
+    @OneToMany
+    @JoinColumn(name = "pet_id")
     private Pet pet;
+
+    @Column(name = "description")
     private String description;
 
     public LocalDate getLocalDate() {
