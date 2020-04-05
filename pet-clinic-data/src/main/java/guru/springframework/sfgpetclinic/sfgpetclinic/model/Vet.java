ef5b7c13ba2d62664d3cd6f1d18937936892ad24
Praveen Kumar <praveenbapuram.explore@gmail.com>
2020-04-05 08:20:35 +0530

    Adding Lombok to the dependency and  use Lombok annotations in all the entities to generate the source code.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package guru.springframework.sfgpetclinic.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vet")
 public class Vet extends Person {
@@ -14,13 +21,7 @@ public class Vet extends Person {
         inverseJoinColumns = @JoinColumn(name="speciality_id")
     )
     private Set<Speciality> specialities =new HashSet<>();
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
 
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 
 
 }
